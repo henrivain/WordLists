10be9f0e59cba09e2cb43f1f6f7ed10bc2d7081b
matikkaeditorinkaantaja <matikkaeditorinkaantaja@gmail.com>
2022-08-09 17:31:38 +0300

    Add viewmodel to word data page

--- WordListsApp/WordListsViewModels/Interfaces/IWordDataViewModel.cs
+++ WordListsApp/WordListsViewModels/Interfaces/IWordDataViewModel.cs
@@ -4,6 +4,7 @@ public interface IWordDataViewModel
     public int WordListCount { get; }
     public int WordCount { get; }
     public int LearnedWordCount { get; }
+    public int MightKnowWordCount { get; }
     public int NeverHeardWordCount { get; }
 
     public IAsyncRelayCommand UpdateData { get; }
--- WordListsApp/WordListsViewModels/WordDataViewModel.cs
+++ WordListsApp/WordListsViewModels/WordDataViewModel.cs
@@ -1,8 +1,28 @@
 ﻿
 namespace WordListsViewModels;
+
 [INotifyPropertyChanged]
 public partial class WordDataViewModel : IWordDataViewModel
 {
+    [ObservableProperty]
+    int wordListCount = 0;
+
+    [ObservableProperty]
+    int wordCount = 0;
+    
+    [ObservableProperty]
+    int learnedWordCount = 0;
+
+    [ObservableProperty]
+    int neverHeardWordCount = 0;
+
+    [ObservableProperty]
+    int mightKnowWordCount = 0;
+
+    public IAsyncRelayCommand UpdateData => new AsyncRelayCommand(async () =>
+    {
+        await Task.Delay(1);
+    });
 
 
 }
